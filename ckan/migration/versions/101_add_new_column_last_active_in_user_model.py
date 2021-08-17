48be68f2e2921b9954713906fa5c2f74f91df46b
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-08-17 22:34:49 +0200

    Add new column in the model and create db migration script

---
+++ ckan/migration/versions/101_add_new_column_last_active_in_user_model.py
@@ -0,0 +1,25 @@
+"""Add new column last_active in User model
+
+Revision ID: d65b0ae235c0
+Revises: ccd38ad5fced
+Create Date: 2021-08-17 22:16:39.626233
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd65b0ae235c0'
+down_revision = 'ccd38ad5fced'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('user', sa.Column('last_active', sa.DateTime))
+
+
+def downgrade():
+    op.drop_column('user', 'last_active')
+
